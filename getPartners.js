765227984186c0b9f5dce5cc03c58a9b1d9d8c72
Delta <46466697+DeltaxHunter@users.noreply.github.com>
2022-10-02 17:05:54 +0200

    new format + script

--- getPartners.js
+++ getPartners.js
@@ -1,32 +1,14 @@
-var data = $0;
+var scripts = document.getElementsByTagName("script");
 
-var partners = [];
+var script = scripts[9].innerText;
+var partners;
 
-for (var i = 0; i < data.childElementCount; i++)
-{
-	var partner = {name:data.children[i].children[0].children[1].children[0].innerHTML};
-	
-	var linkData = data.children[i].children[0].children[1].children[1].children;
-	
-	//console.log(linkData);
-	
-	for (var j = 0; j < linkData.length; j++)
-	{
-		var link = linkData[j].children[0];
-		
-		//console.log(link);
-		
-		switch (link.title)
-		{
-			case "youtube": partner.youtube = link.href; break;
-			case "twitch": partner.twitch = link.href; break;
-			case "website": partner.website = link.href; break;
-		}
-	}
-	
-	partners.push(partner);
-}
+var start = script.indexOf("partners = [{");
 
-partners.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+script = script.substring(start + "partners = ".length);
 
-console.log(partners);
+var end = script.indexOf("}}],");
+
+script = script.substring(0, end + 3);
+
+var partners = JSON.parse(script);
