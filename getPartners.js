68322805bd5725d0a73bdc9726a607092debfa81
Delta <46466697+DeltaxHunter@users.noreply.github.com>
2022-10-05 21:53:21 +0200

    remove avatars
    avatars update daily, difficult to track changes

--- getPartners.js
+++ getPartners.js
@@ -13,4 +13,9 @@ script = script.substring(0, end + 3);
 
 var partners = JSON.parse(script);
 
+for (var i = 0; i < partners.length; i++)
+{
+	delete partners[i].avatar;
+}
+
 console.log(partners);
