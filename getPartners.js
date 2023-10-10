6658e56c1c80141efde6f4b62454424e3cfbec12
Delta <46466697+DeltaGW2@users.noreply.github.com>
2023-10-10 15:41:55 +0200

    script update for new page

--- getPartners.js
+++ getPartners.js
@@ -1,13 +1,13 @@
 var scripts = document.getElementsByTagName("script");
 
-var script = scripts[9].innerText;
+var script = scripts[8].innerText;
 var partners;
 
 var start = script.indexOf("partners = [{");
 
 script = script.substring(start + "partners = ".length);
 
-var end = script.indexOf("}}],");
+var end = script.indexOf("}}];");
 
 script = script.substring(0, end + 3);
 
