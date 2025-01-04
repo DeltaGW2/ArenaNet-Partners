4c00b17b742ff4d593728464f0e7b4cffc3bd16e
Delta <46466697+DeltaGW2@users.noreply.github.com>
2025-01-04 20:06:05 +0100

    Update getPartnersJSDOM.js

--- getPartnersJSDOM.js
+++ getPartnersJSDOM.js
@@ -8,19 +8,41 @@ const htmlContent = fs.readFileSync('page.html', 'utf-8');
 const dom = new JSDOM(htmlContent);
 const document = dom.window.document;
 
-// Extract the script and parse the partners data
+// Get all script tags
 var scripts = document.getElementsByTagName("script");
-var script = scripts[8].innerText;
-var start = script.indexOf("partners = [{");
-script = script.substring(start + "partners = ".length);
-var end = script.indexOf("}}];");
-script = script.substring(0, end + 3);
-var partners = JSON.parse(script);
 
-// Remove avatars from partners
-for (var i = 0; i < partners.length; i++) {
-    delete partners[i].avatar;
+// Debugging: Log the total number of scripts
+console.log(`Found ${scripts.length} script tags.`);
+
+// Look for an inline script containing "partners = [{"
+let partnersScript = null;
+for (let i = 0; i < scripts.length; i++) {
+    if (scripts[i].innerHTML.includes("partners = [{")) {
+        partnersScript = scripts[i].innerHTML;
+        console.log(`Found target script at index ${i}`);
+        break;
+    }
 }
 
-// Output the modified partners array as JSON
-console.log(JSON.stringify(partners, null, 2));
+if (partnersScript) {
+    // Extract the partners data
+    var start = partnersScript.indexOf("partners = [{");
+    partnersScript = partnersScript.substring(start + "partners = ".length);
+    var end = partnersScript.indexOf("}}];");
+    if (end !== -1) {
+        partnersScript = partnersScript.substring(0, end + 3);
+        var partners = JSON.parse(partnersScript);
+
+        // Remove avatars from partners
+        for (var i = 0; i < partners.length; i++) {
+            delete partners[i].avatar;
+        }
+
+        // Output the modified partners array as JSON
+        fs.writeFileSync('partners.json', JSON.stringify(partners, null, 2));
+        console.log('Successfully extracted and processed partners data.');
+    } else {
+        console.error('Failed to find the end of the partners data.');
+    }
+} else {
+    co
